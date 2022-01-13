e56363e012c7b80ecc14a4e3a21b32cd18f1a6ec
elw55555 <89215293+elw55555@users.noreply.github.com>
2022-01-12 17:44:48 -0800

    Update MainTests.java

--- src/test/java/com/nighthawk/intellijs/MainTests.java
+++ src/test/java/com/nighthawk/intellijs/MainTests.java
@@ -3,7 +3,7 @@ package com.nighthawk.csa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Main.class)
 class MainTests {
 
     @Test
