a93910b09e2ee9caf39ce0e497c9a6c9bbcf243b
ericl <ericliwang55555@gmail.com>
2022-01-20 09:09:20 -0800

    Eric Unit 6 FRQ

---
+++ src/main/java/FRQS/Unit6EW.java
@@ -0,0 +1,16 @@
+package FRQS;
+
+public class Unit6EW {
+    public static void main(String[] args) {
+        //initialize array
+        String words[] = {"ten", "fading", "post", "card", "thunder", "hinge", "trailing", "batting"};
+        //enhanced for loop to loop through the array
+        for (String word : words) {
+            //lastIndexOf() method returns the position of the last found occurrence
+            //of the substring specified in a string
+            if (word.lastIndexOf("ing") == word.length() - 3) {
+                System.out.println(word);
+            }
+        }
+    }
+}
