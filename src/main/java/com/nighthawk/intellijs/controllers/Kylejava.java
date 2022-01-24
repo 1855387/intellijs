38ccac5aafbc428c77c64cde615a79422282d520
kylem <kylem31415@gmail.com>
2022-01-24 09:04:44 -0800

    Fixed kyleabout controller and imports

--- src/main/java/com/nighthawk/intellijs/controllers/Kylejava.java
+++ src/main/java/com/nighthawk/intellijs/controllers/Kylejava.java
@@ -1,9 +1,24 @@
-package com.example.sping_portfolio.controllers;
+package com.nighthawk.intellijs.controllers;
 
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.ui.Model;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class Kylejava {
